4f8ed5e1fb22e251447136249bc2726671148100
SimonKopischke <118012445+SimonKopischke@users.noreply.github.com>
2026-07-28 09:26:17 -0500

    Add files via upload

---
+++ Simulation Files/Calculate Output from Sims.R
@@ -0,0 +1,112 @@
+install.packages("PRROC")
+require(PRROC)
+
+data_results <- matrix(0, nrow =  4 * 6 * 5 * 10, ncol = 9)
+index <- 1
+colnames(data_results) <- c("d", "omega", "p", "iteration", "AUPRC", "Coverage", "Power", "Size", "FDR")
+for (d in c(1,4,8,12)) {
+  for (omega in c(0.1,0.2,0.4,0.5,0.7,0.8)) {
+    for (p in c(0.1,0.3,0.5,0.7,0.9)) {
+      for (i in 1:10) {
+        data_results[index, 1:4] <- c(d, omega, p, i)
+        dir <- paste0("/Users/simonkopischke/Desktop/REU Biostats Project/new_model/sim_results_d", d, "_w", omega, "_p", p, "_", i)
+        # if (!file.exists(dir)) {
+        #   next
+        # }
+        # if (!file.exists(paste0(dir, "/pip.csv"))) {
+        #   next
+        # }
+        causal_variants <- scan(paste0(dir, "/sim1000G_cat_causalvar.txt"), sep = " ", quiet = TRUE)
+        pip <- read.csv(paste0(dir, "/pip.csv"))
+        if (file.size(paste0(dir, "/credible_set.txt")) == 0) {
+          cred_sets <- matrix(0, nrow = 1, ncol = 1)
+          matching <- 0
+          sets_with_causal <- 0
+        } else {
+          cred_sets <- read.table(paste0(dir, "/credible_set.txt"), header = FALSE, sep = " ", fill = TRUE) + 1
+          matching <- sum(unlist(cred_sets) %in% causal_variants)
+          condit_prob <- read.table(paste0(dir, "/conditional_credible_variants_probability.txt"), header = FALSE, sep = " ", fill = TRUE)
+          sets_with_causal <- rep(F, dim(cred_sets)[1])
+          for (j in 1:dim(cred_sets)[1]) { # number of variants within each credible set
+            matching_in_set <- sum(cred_sets[j,] %in% causal_variants)
+            binary <- ifelse(matching_in_set > 0, T, F)
+            sets_with_causal[j] <- binary
+          }
+        }
+        
+        # AUPRC
+        # pip <- pip[na.exclude(unlist(cred_sets)),]
+        # pip_index <- na.exclude(unlist(cred_sets)) %in% causal_variants
+        # pr <- pr.curve(scores.class0=pip[pip_index,2],
+        #                scores.class1=pip[!pip_index,2],
+        #                curve=T)
+        pr <- pr.curve(scores.class0=pip[causal_variants,2],
+                       scores.class1=pip[-causal_variants,2],
+                       curve=T)
+        data_results[index, 5] <- pr$auc.integral
+        
+        # Coverage
+        data_results[index, 6] <- sum(sets_with_causal) / dim(cred_sets)[1]
+        # data_results[index, 6] <- mean(na.exclude(unlist(condit_prob)))
+        # data_results[index, 6] <- mean(condit_prob[sets_with_causal,1])
+        # if (is.nan(data_results[index, 6])) {
+        #   data_results[index, 6] <- 0
+        # }
+        
+        # Power
+        data_results[index, 7] <- matching / d
+        
+        # Size
+        data_results[index, 8] <- sum(!is.na(cred_sets)) / dim(cred_sets)[1]
+        
+        # FDR
+        data_results[index, 9] <- matching / sum(!is.na(cred_sets))
+        
+        index <- index + 1
+        # print(paste(d, omega, p, i))
+      }
+    }
+  }
+}
+
+# preliminary graphs
+d_graph <- matrix(0, ncol = 4, nrow = 4)
+rownames(d_graph) <- c(1, 4, 8, 12)
+colnames(d_graph) <- c("AUPRC", "Coverage", "Power", "Size")
+d_graph[1,] <- colMeans(data_results[which(data_results[,1] == 1,),5:8])
+d_graph[2,] <- colMeans(data_results[which(data_results[,1] == 4,),5:8])
+d_graph[3,] <- colMeans(data_results[which(data_results[,1] == 8,),5:8])
+d_graph[4,] <- colMeans(data_results[which(data_results[,1] == 12,),5:8])
+for (i in 1:(dim(d_graph)[2] - 1)) {
+  plot(d_graph[,i], type = "l", xlab = "d", ylab = colnames(d_graph)[i], ylim = c(0,1))
+}
+plot(d_graph[,4], type = "l", xlab = "d", ylab = colnames(d_graph)[4], ylim = c(0,10))
+
+omega_graph <- matrix(0, ncol = 4, nrow = 6)
+rownames(omega_graph) <- c(0.1, 0.2, 0.4, 0.5, 0.7, 0.8)
+colnames(omega_graph) <- c("AUPRC", "Coverage", "Power", "Size")
+omega_graph[1,] <- colMeans(data_results[which(data_results[,2] == 0.1,),5:8])
+omega_graph[2,] <- colMeans(data_results[which(data_results[,2] == 0.2,),5:8])
+omega_graph[3,] <- colMeans(data_results[which(data_results[,2] == 0.4,),5:8])
+omega_graph[4,] <- colMeans(data_results[which(data_results[,2] == 0.5,),5:8])
+omega_graph[5,] <- colMeans(data_results[which(data_results[,2] == 0.7,),5:8])
+omega_graph[6,] <- colMeans(data_results[which(data_results[,2] == 0.8,),5:8])
+for (i in 1:(dim(omega_graph)[2] - 1)) {
+  plot(omega_graph[,i], type = "l", xlab = "omega", ylab = colnames(omega_graph)[i], ylim = c(0,1))
+}
+plot(omega_graph[,4], type = "l", xlab = "omega", ylab = colnames(omega_graph)[4], ylim = c(0,10))
+
+p_graph <- matrix(0, ncol = 4, nrow = 5)
+rownames(p_graph) <- c(0.1, 0.3, 0.5, 0.7, 0.9)
+colnames(p_graph) <- c("AUPRC", "Coverage", "Power", "Size")
+p_graph[1,] <- colMeans(data_results[which(data_results[,3] == 0.1,),5:8])
+p_graph[2,] <- colMeans(data_results[which(data_results[,3] == 0.3,),5:8])
+p_graph[3,] <- colMeans(data_results[which(data_results[,3] == 0.5,),5:8])
+p_graph[4,] <- colMeans(data_results[which(data_results[,3] == 0.7,),5:8])
+p_graph[5,] <- colMeans(data_results[which(data_results[,3] == 0.9,),5:8])
+for (i in 1:(dim(p_graph)[2] - 1)) {
+  plot(p_graph[,i], type = "l", xlab = "p", ylab = colnames(p_graph)[i], ylim = c(0,1))
+}
+plot(p_graph[,4], type = "l", xlab = "p", ylab = colnames(p_graph)[4], ylim = c(0,10))
+
+write.table(data_results, file = "/Users/simonkopischke/Desktop/REU Biostats Project/new_model/data_results.txt", row.names = F, sep = " ")
---
+++ Simulation Files/run_loop new model.sh
@@ -0,0 +1,55 @@
+SIM_DIR="/Users/simonkopischke/Desktop/REU Biostats Project"
+BEATRICE_DIR="/Users/simonkopischke/Desktop/REU Biostats Project/Beatrice-Finemapping"
+RESULTS_DIR="/Users/simonkopischke/Desktop/REU Biostats Project/new_model"
+Z_DIR=${SIM_DIR}
+LD_DIR=${SIM_DIR}
+
+# d in 1 4 8 12
+# omega in 0.1 0.2 0.4 0.5 0.7 0.8
+# p in 0.1 0.3 0.5 0.7 0.9
+# i in {11..20}
+
+for d in 1 4 8 12
+do
+	for omega in 0.1 0.2 0.4 0.5 0.7 0.8
+	do
+		for p in 0.1 0.3 0.5 0.7 0.9
+		do
+			for i in {11..20}
+			do
+				if [ -e "${RESULTS_DIR}/sim_results_d${d}_w${omega}_p${p}_${i}/credible_set.txt" ]
+				then
+					continue
+				fi
+
+				echo d${d}_w${omega}_p${p}_${i}
+				
+				cd "$SIM_DIR"
+				Rscript "New Performance Analysis_test.R" ${d} ${omega} ${p}
+
+				cd "$RESULTS_DIR"
+				mkdir -p "sim_results_d${d}_w${omega}_p${p}_${i}"
+
+				cd "$SIM_DIR"
+
+ 				ls -lh "${SIM_DIR}/sim1000G_cat_zscores.csv"
+ 				ls -lh "${SIM_DIR}/sim1000G_cat_LD.csv"
+				ls -lh "${SIM_DIR}/sim1000G_cat_causalvar.txt"
+		
+				echo "renaming causal_v files and moving..."
+				mv "${SIM_DIR}/sim1000G_cat_causalvar.txt" "${RESULTS_DIR}/sim_results_d${d}_w${omega}_p${p}_${i}/sim1000G_cat_causalvar.txt"
+
+				cd "$BEATRICE_DIR"
+		
+				ls -lh "${Z_DIR}/sim1000G_cat_zscores.csv"
+				ls -lh "${LD_DIR}/sim1000G_cat_LD.csv"
+
+				python3 beatrice.py --z "${Z_DIR}/sim1000G_cat_zscores.csv" --LD "${LD_DIR}/sim1000G_cat_LD.csv" --N 5000 --target "${RESULTS_DIR}/sim_results_d${d}_w${omega}_p${p}_${i}"
+
+				# removing files
+				rm -f "${SIM_DIR}/sim1000G_cat_zscores.csv"
+ 				rm -f "${SIM_DIR}/sim1000G_cat_LD.csv"
+			done
+		done
+	done
+done
